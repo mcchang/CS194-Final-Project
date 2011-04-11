25591f5e6497d523c2ce8e6dc5233295f5a4a8e2
Michael Chang <aznm1ke31@gmail.com>
2011-04-10 21:53:43 -0700

    adding files

---
+++ server.py
@@ -0,0 +1,41 @@
+import os
+import tornado.ioloop
+import tornado.web
+
+settings = {
+    "static_path": os.path.join(os.path.dirname(__file__), "static"),
+    "template_path": os.path.join(os.path.dirname(__file__), "templates"),
+    "debug": True
+}
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("main.html")
+
+
+class VisHandler(tornado.web.RequestHandler):
+    def get(self):
+        template = self.request.path[1:] + ".html"
+        self.render(template) 
+
+
+class DataHandler(tornado.web.RequestHandler):
+    def get(self):
+        data_filename = self.request.path.split("/")[-1]
+        data_filepath = os.path.join(settings["static_path"], data_filename)
+        data_file = open(data_filepath, 'r')
+        data = data_file.read()
+        self.write(data)
+        data_file.close()
+
+
+application = tornado.web.Application([
+    (r"/", MainHandler),
+    (r"/data/.+\.json", DataHandler),
+    (r"/.+", VisHandler)
+], **settings)
+
+
+if __name__ == "__main__":
+    application.listen(8080)
+    tornado.ioloop.IOLoop.instance().start()
